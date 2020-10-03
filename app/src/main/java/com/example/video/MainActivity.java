893e58a3e27d7ba0c273d4fdde3e6d70c4f9979c
Shubham <shubhamppt1170@gmail.com>
2020-10-03 14:07:10 +0530

    begin

---
+++ app/src/main/java/com/example/video/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView vid = (VideoView) findViewById(R.id.videoView);
+        vid.setVideoPath("android.resources//" + getPackageName() +"/" +R.raw.demovideo);
+        vid.start();
+    }
+}
